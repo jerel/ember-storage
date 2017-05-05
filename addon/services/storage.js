c11e964cc23154b62d2e4e08e5091e33dd605bca
Michael Padon <maprules1000@gmail.com>
2017-05-05 16:11:03 -0500

    implements a fix for test-breaking changes in 2.12.0-beta.1

--- addon/services/storage.js
+++ addon/services/storage.js
@@ -34,6 +34,13 @@ export default Ember.Service.extend({
     let key = this._prefix(k),
         type = this.get('type');
 
+    // an inelegant solution for behavior introduced in Ember 2.12.0-beta.1
+    // fix introduced in Ember 2.14.0-beta.1
+    //https://github.com/emberjs/ember.js/commit/652adeee2d34c0ae1855612b1d26916eb7c3e40a#diff-83aef7046279fbc5512d665c03bff129
+    if (k.includes('NAME_KEY') || k.includes('OWNER')){
+      return;
+    }
+
     if(Ember.isNone(value)) {
       delete storage[type][key];
     } else {
